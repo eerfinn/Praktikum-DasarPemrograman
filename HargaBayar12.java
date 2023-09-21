a7ea3c2de5ac3d75747bee79c7d9940a26cdd192
Erfin Jauhari Dwi Brian <erfinbrian@gmail.com>
2023-09-21 12:09:37 +0700

    Modifikasi Percobaan 3

--- HargaBayar12.java
+++ HargaBayar12.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 public class HargaBayar12 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int harga, jumlah;
-        double diskon=0.1, total, bayar, jumlahDiskon;
+        String merekBuku;
+        int harga, jumlah, jumlahHalaman;
+        double diskon, total, bayar, jumlahDiskon;
         
+        System.out.println("Masukan Merek Buku yang dibeli ");
+        merekBuku=input.nextLine();
+        System.out.println("Masukan Jumlah halaman buku yang dibeli ");
+        jumlahHalaman=input.nextInt();
         System.out.println("Masukan harga barang yang dibeli ");
         harga=input.nextInt();
         System.out.println("Masukan jumlah barang yang dibeli ");
         jumlah=input.nextInt();
+        System.out.println("Masukan diskon barang yang dibeli ");
+        diskon=input.nextDouble();
 
         total= harga * jumlah;
         jumlahDiskon= total * diskon;
