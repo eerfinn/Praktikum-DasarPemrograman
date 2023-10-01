09fd8d1438a575f81c3ce2faaa198545e4a60987
Erfin Jauhari Dwi Brian <erfinbrian@gmail.com>
2023-10-01 14:17:39 +0700

    Modifikasi Percobaan 1

--- Jobsheet 5/PemilihanPercobaan12.java
+++ Jobsheet 5/PemilihanPercobaan12.java
@@ -4,13 +4,17 @@ public class PemilihanPercobaan12 {
     public static void main(String[] args) {
         Scanner input12 = new Scanner(System.in);
          
+        String hasil;
+
         System.out.print("Masukan angka: ");
         int angka = input12.nextInt();
         
-        if (angka % 2 == 0)
-            System.out.println("Angka " + angka + " Adalah Bilanga Genap");
-
-        else
-            System.out.println("Angka " + angka + "Adalah Bilangan Ganjil");
+        if (angka % 2 == 0) {
+            hasil = "Bilangan Genap";
+        }
+        else{
+            hasil = "Bilangan Ganjil";
+        }
+        System.out.println(angka + " adalah " + hasil);
     }
 }
